005a807920fecb7481c142ad5bfec8c93b2ef0a7
Deby2404 <dedede2404@gmail.com>
2022-03-13 11:25:23 +0000

    [add]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,31 @@
 class BooksController < ApplicationController
   def new
+   @book = Book.new
+  end
 
+  def create
+    @book = Book.new(book_params)
+    @book = user.id = current_user.id
+    if @book.save
+      redirect_to books_path
+    else
+      render :new
+    end
   end
 
   def index
+    @book = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = book.find(params[:id])
   end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
+  end
+
 end
