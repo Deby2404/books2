3e3630d079a41521835d03cc87465b8f4f6bcaef
Deby2404 <dedede2404@gmail.com>
2022-03-19 05:42:09 +0000

    add layouts

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,11 +4,11 @@ class BooksController < ApplicationController
   def create
     @user = current_user
     @books = Book.all
-    @new_book = Book.new
+    @new_book = Book.new(book_params)
     @new_book.user_id = current_user.id
     if @new_book.save
     flash[:notice]="You have created book successfully."
-    redirect_to book_path(@book)
+    redirect_to book_path(@new_book)
     else
       render :index
     end
@@ -55,6 +55,6 @@ class BooksController < ApplicationController
 
   private
   def book_params
-    params.require(:book).permit(:title, :body, :user_id, :profile_image)
+    params.require(:book).permit(:title, :body)
   end
 end
