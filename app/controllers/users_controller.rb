128fcefe966087804a9fd7c2529d1fedd1fe7d2a
Deby2404 <dedede2404@gmail.com>
2022-03-19 23:52:41 +0000

    fix errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     if @user == current_user
       render "edit"
     else
-      redirect_to user_path
+      redirect_to user_path(current_user.id)
     end
   end
 
